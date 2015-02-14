c41cbfa800f79459eb8ba50dc59f805f3c86a25d
Kent Howard <kenthow@gmail.com>
2015-02-14 17:32:55 -0600

    Commit before adding slug field to models.py

--- rango/models.py
+++ rango/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+#from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    #slug = models.SlugField(unique=True)
     
     def __string__(self):
         return self.name
--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category
+
 
 def index(request):
+    '''
+    Query the db for a list of ALL categories currently stored.
+    Order the categories by no. likes in descending order.
+    Retrieve the top 5 only - or all if less than 5.
+    Place the list in our context_dict dictionary which will
+    be passed to the template engine.
+    '''
+    category_list = Category.objects.order_by('-likes')
+    context_dict = {'categories': category_list}
 
-    context_dict = {'boldmessage': 'I am bold font from the context.'}
+    # Render the response and send it back.
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
